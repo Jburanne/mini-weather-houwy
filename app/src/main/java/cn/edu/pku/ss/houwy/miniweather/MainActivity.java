e381a6563f0dcd4c9ec55dce6c3d1af5372a9f14
houwy <houwyvenny@126.com>
2018-10-04 20:29:21 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/ss/houwy/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.ss.houwy.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+
+}
